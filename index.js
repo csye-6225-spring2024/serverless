a0cf7162d44553d0743d790905e8095b8e199b69
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-27 17:19:47 -0400

    Added mail logic

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { PubSub } from '@google-cloud/pubsub';
 import { v4 as uuidv4 } from 'uuid';
-import moment from 'moment';
-import axios from 'axios';
 import mailgun from 'mailgun-js';
 import { Sequelize, DataTypes } from 'sequelize';
 
@@ -68,17 +66,17 @@ const mg = mailgun({
 const pubsub = new PubSub();
 const currentTime = new Date();
 
-export const handleNewUserAccount = async (event, context) => {
+export const processNewUser = async (event, context) => {
   const pubsubMessage = event.data ? JSON.parse(Buffer.from(event.data, 'base64').toString()) : {};
   const { id, username } = pubsubMessage;
   const token = uuidv4();
-  await sendVerificationEmail(username, token);
+  await VerificationEmail(username, token);
   const validity = new Date(Date.now() + 2 * 60 * 1000);
   await updateDatabase(username, token, validity);
   console.log(`Verification email sent to ${username}`);
 };
 
-const sendVerificationEmail = async (username, token) => {
+const VerificationEmail = async (username, token) => {
   try {
     const apiKey = "d6fba5dc7e62e2c6cac695f04613b321-309b0ef4-621410c1";
     const domain = "centralhub.me";
@@ -87,7 +85,7 @@ const sendVerificationEmail = async (username, token) => {
       from: sender,
       to: username,
       subject: 'Email Verification',
-      text: `Please click the following link to verify your email: ${generateVerificationLink(username, token)}`,
+      text: `Please click the following link to verify your email: ${VerificationLink(username, token)}`,
     };
     const body = await mg.messages().send(data);
     console.log('Mailgun response:', body);
@@ -97,7 +95,7 @@ const sendVerificationEmail = async (username, token) => {
   }
 };
 
-const generateVerificationLink = (username, token) => {
+const VerificationLink = (username, token) => {
   const baseUrl = 'http://centralhub.me:8080';
   const verificationLink = `${baseUrl}/verify?username=${username}&token=${token}`;
   return verificationLink;
